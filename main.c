fbc7185681b83f2e6ef922a2437e0060d9ecd339
kmhhf <kmhhf@umsl.edu>
2020-02-10 19:41:35 -0600

    added more error checking and comments

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+//Kyle Moore
+//02/03/2020
+//This program will list all files in a directory and sub directories using a breadth first search.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,7 +23,9 @@ int main(int argc, char* argv[])
     char* cwd = directory;
     char* next = (char*) malloc(255);
     char* simpleDirectory;
+    DIR *dir;
 
+    //Handles all of the options by setting variables to 1 if the option is used.
     while ((opt = getopt(argc, argv, "hLtpiugsdl")) != -1)
     {
         switch(opt)
@@ -35,7 +41,7 @@ int main(int argc, char* argv[])
                 printf("        g - Print the GID associated with the file\n");
                 printf("        s - Print the file size in bytes\n");
                 printf("        d - Show the time of last modification\n");
-                printf("        l - This option prints information on the file as if the options tpiugs are all specified\n");
+                printf("        l - This option prints information on the file as if the options tpiugs are a                                                                                                                ll specified\n");
                 exit(EXIT_FAILURE);
             case 'L':
                 L = 1;
@@ -70,13 +76,26 @@ int main(int argc, char* argv[])
         }
     }
 
+    //checks to see if a command line arguement was given and uses the arguement as the starting directory
     if(argv[optind] != NULL)
     {
-        chdir(argv[optind]);
+        if(chdir(argv[optind]) != 0)  //error check for chdir
+        {
+            fprintf(stderr, "%s: Error: ", argv[0]);
+            perror("");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    getcwd(directory, sizeof(directory));
+    //gets the current directory and checks for errors
+    if(getcwd(directory, sizeof(directory)) == NULL)
+    {
+        fprintf(stderr, "%s: Error: ", argv[0]);
+        perror("");
+        exit(EXIT_FAILURE);
+    }
 
+    //if the directory isnt root, sets the starting directory to a variable to use in the display of the results
     if (strcmp(directory, "/") != 0)
     {
         simpleDirectory = strrchr(cwd, '/');
@@ -84,30 +103,62 @@ int main(int argc, char* argv[])
     }
 
     que = malloc(sizeof(struct queue));
-    createQueue(que);
-    enqueue(que, cwd);
 
-    while (que->first != NULL)
+    if(que == NULL)
     {
-        cwd = dequeue(que);
-        chdir(cwd);
+        fprintf(stderr, "%s: Error: ", argv[0]);
+        perror("");
+        exit(EXIT_FAILURE);
+    }
 
-        DIR *dir = opendir(cwd);
-        while ((dirInfo = readdir(dir)) != NULL)
+    createQueue(que);               //creates the queue to hold the directories that needed to be checked
+    enqueue(que, cwd);              //puts the starting directory in the queue
+
+    while (que->first != NULL)      //runs as long as the queue is not empty
+    {
+        cwd = dequeue(que);     //gets next directory from the que
+        if(chdir(cwd) != 0)     //changes current working directory to the next one in the que
+        {
+            fprintf(stderr, "%s: Error: ", argv[0]);
+            perror("");
+            exit(EXIT_FAILURE);
+        }
+
+        dir = opendir(cwd);     //opens the next directry and error check
+        if(dir == NULL)
+        {
+            fprintf(stderr, "%s: Error: ", argv[0]);
+            perror("");
+            exit(EXIT_FAILURE);
+        }
+
+        while ((dirInfo = readdir(dir)) != NULL)   //goes through each file in the directory
         {
             struct stat statbuf;
 
-            if (strcmp(dirInfo->d_name, ".") != 0 && strcmp(dirInfo->d_name, "..") != 0)
+            if (strcmp(dirInfo->d_name, ".") != 0 && strcmp(dirInfo->d_name, "..") != 0)  //removes . and .. from                                                                                                                 the results
             {
+                //uses stat if the L option is set in order to follow soft links
                 if ( L == 1 )
                 {
-                    stat(dirInfo->d_name, &statbuf);
+                    if(stat(dirInfo->d_name, &statbuf) != 0)  //get file info
+                    {
+                        fprintf(stderr, "%s: Error: ", argv[0]);
+                        perror("");
+                        exit(EXIT_FAILURE);
+                    }
                 }
                 else
                 {
-                    lstat(dirInfo->d_name, &statbuf);
+                    if(lstat(dirInfo->d_name, &statbuf) != 0)  //get file info without following links
+                    {
+                        fprintf(stderr, "%s: Error: ", argv[0]);
+                        perror("");
+                        exit(EXIT_FAILURE);
+                    }
                 }
 
+                //check for direcories and add them to the queue
                 if (S_ISDIR(statbuf.st_mode) != 0)
                 {
                     strcpy(next, cwd);
@@ -116,6 +167,7 @@ int main(int argc, char* argv[])
                     enqueue(que, next);
                 }
 
+                //if t option was used, check file permissions and prints them
                 if (t == 1)
                 {
                     if (S_ISDIR(statbuf.st_mode) != 0)
@@ -221,23 +273,27 @@ int main(int argc, char* argv[])
                     printf(" ");
                 }
 
+                //if i option was used, checks the number of links and prints the result
                 if( i == 1)
                 {
                     printf("%*i ", 3, statbuf.st_nlink);
                 }
 
+                //if u option was used prints out the UID
                 if( u == 1)
                 {
                     struct passwd *uid = getpwuid(statbuf.st_uid);
                     printf("%*s ", 8, uid->pw_name);
                 }
 
+                //if g option was used prints out the GID
                 if( g == 1)
                 {
                     struct group *gid = getgrgid(statbuf.st_gid);
                     printf("%*s ", 8, gid->gr_name);
                 }
 
+                //if s option was used prints out the size of the file
                 if( s == 1)
                 {
                     long long size = statbuf.st_size;
@@ -259,6 +315,7 @@ int main(int argc, char* argv[])
                     }
                 }
 
+                //if d option was used prints out the last time the file was modified
                 if( d == 1)
                 {
                     char time[40];
@@ -266,7 +323,7 @@ int main(int argc, char* argv[])
                     printf("%*s ", 10, time);
                 }
 
-                printf("%s/%s\n", strstr(cwd, simpleDirectory), dirInfo->d_name);
+                printf("%s/%s\n", strstr(cwd, simpleDirectory), dirInfo->d_name); //print name and relative p                                                                                                                ath of file
             }
         }
         close(dir);
--- queue.c
+++ queue.c
@@ -1,33 +1,55 @@
+// Kyle Moore
+// 02/03/20
+// This file creates a queue ADT
+
 #include "queue.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
+//Create an empty queue by setting both pointers to NULL
 void createQueue(struct queue* que)
 {
     que->first = NULL;
     que->last = NULL;
 }
 
+//Add a node to the queue
 void enqueue(struct queue* que, char* directory)
 {
     struct node* temp;
-    temp = malloc(sizeof(struct node));
-    temp->directory = directory;
+    char* dir = malloc(strlen(directory) + 1);  //allocate memory for the name of the directory to be added
+    if(dir == NULL)                             //malloc error check
+    {
+        perror("Error: ");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(dir, directory);
+    temp = malloc(sizeof(struct node));         //memeory for the node
+    if(temp == NULL)                            //error check
+    {
+        perror("Error: ");
+        exit(EXIT_FAILURE);
+    }
+
+    temp->directory = dir;
     temp->next = NULL;
 
-    if (que->first == NULL)
+    if (que->first == NULL)                     //add a node to an empty queue
     {
         que->first = temp;
         que->last = temp;
     }
 
-    else
+    else                                        //add a node to the end of a queue
     {
         que->last->next = temp;
         que->last = temp;
     }
 }
 
+//remove a node from the queue and return the value stored in directory
 char* dequeue(struct queue* que)
 {
     struct node* temp;
